8ff685e6bfb5668b81ea044a5dc7e6215ab14a21
Zephyie <ssourav262011@gmail.com>
2026-05-23 14:06:34 +0530

    Update c-realV2.py

--- c-realV2.py
+++ c-realV2.py
@@ -71,12 +71,7 @@ fetching_members = False
 bad_filename_map = dict((ord(char), None) for char in '<>:"\\/|?*')
 grant_all_permissions = False
 # normal functions==============
-def exit():
-    try:
-        # input('Press enter to exit...')
-    except (EOFError, KeyboardInterrupt):
-        pass
-    sys.exit(1)
+def exit()
 
 def banner():
     """Handler for non-unicode consoles"""
